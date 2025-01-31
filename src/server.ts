2e272ee48b906af746eb88cd3020548bd104e116
byigitt <137426514+byigitt@users.noreply.github.com>
2025-01-31 11:46:41 +0300

    feat: integrate gRPC server with Fastify application

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,28 @@
 import Fastify from 'fastify';
 import fp from 'fastify-plugin';
 import { dbConfig } from './config/database';
+import * as grpc from '@grpc/grpc-js';
+import * as protoLoader from '@grpc/proto-loader';
+import path from 'path';
+import { BookService } from './services/book.service';
 
 const fastify = Fastify({
   logger: true,
 });
 
 fastify.register(require('@fastify/postgres'), dbConfig);
+
+const PROTO_PATH = path.resolve(__dirname, 'proto/book.proto');
+const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+});
+
+const protoDescriptor = grpc.loadPackageDefinition(packageDefinition) as any;
+const bookProto = protoDescriptor.book;
 
 const start = async () => {
   try {
@@ -17,6 +33,21 @@ const start = async () => {
       : `http://${address?.address}:${address?.port}`;
     
     fastify.log.info(`Server running on ${serverAddress}`);
+
+    const grpcServer = new grpc.Server();
+    grpcServer.addService(bookProto.BookService.service, new BookService(fastify));
+    
+    grpcServer.bindAsync(
+      '0.0.0.0:50051',
+      grpc.ServerCredentials.createInsecure(),
+      (error, port) => {
+        if (error) {
+          throw error;
+        }
+
+        fastify.log.info(`gRPC Server running on 0.0.0.0:${port}`);
+      }
+    );
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
